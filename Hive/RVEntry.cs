b11b4bee82c59bdcc977ea81c2fdcadfe073ad58
IsGoose <47721300+IsGoose@users.noreply.github.com>
2023-03-29 04:16:51 +0100

    Set up Libraries

--- Hive/RVEntry.cs
+++ Hive/RVEntry.cs
@@ -1,6 +1,18 @@
-﻿namespace Hive
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using RGiesecke.DllExport;
+using ArmaTools.ArrayParser;
+
+namespace Hive
 {
     public class RVEntry
     {
+        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize,
+            [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            outputSize--;
+        }
+        
     }
 }
