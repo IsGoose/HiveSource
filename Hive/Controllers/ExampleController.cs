05d29fc72aea6d3edd12ade63fcd26ec2db426ac
IsGoose <47721300+IsGoose@users.noreply.github.com>
2023-04-01 04:17:52 +0100

    Add Simple Example Controller & To HiveRunner

---
+++ Hive/Controllers/ExampleController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ArmaTools.ArrayParser;
+using ArmaTools.ArrayParser.DataTypes;
+using Hive.Application;
+using Hive.Application.Attributes;
+using Hive.Application.Enums;
+
+namespace Hive.Controllers;
+
+public class ExampleController
+{
+    /*
+     * Example Table Structure: exampletable1 *
+     * Column Name - DataType   Attributes
+     Id -               VARCHAR(36) PRIMARY KEY NOT NULL
+     ExampleString -    VARCHAR(35)
+     ExampleNumber -    DOUBLE
+     ExampleArray -     TEXT
+     DateCreated -      DATETIME
+     Deleted -          TINYINT
+     DeletedAt -        DATETIME
+     */
+    [Synchronous]
+    public static string Create(ArmaArray data)
+    {
+        //Data = ["ExampleString",50.25,[1,"Hello",2.5,false,"World"]]  as Received from Arma
+        //So We Need to Insert Id & DateCreated, and also return Id
+        var id = Guid.NewGuid().ToString();
+        data.Prepend(new ArmaString(id));
+        data.Append(new ArmaString(DateTime.Now.ToArmaString()));
+        
+        IoC.DBInterface.DbInsert("exampletable1",data);
+        return id;
+    }
+    
+    [Synchronous]
+    public static ArmaArray GetAll()
+    {
+        //We Need All Entries from Table, and we Always need the Result Format as a Multi-Dim Array
+        return IoC.DBInterface.DbRead("exampletable1","*","*",ArrayDimensionOptions.ForceMultiDimension);
+    }
+    
+    [Synchronous]
+    public static ArmaArray GetSingle(string id)
+    {
+        //We Need 1 Entry from Table. Id is Received from Arma
+        return IoC.DBInterface.DbRead("exampletable1","*",$"`Id` = '{id}'");
+    }
+    
+    public static void Update(ArmaArray data)
+    {
+        //We Need Update the ExampleString Column, but not any other column
+        //Data = ["imagineAPrimaryKey","ExampleStringUpdated",nil,nil,nil] as Received from Arma
+        IoC.DBInterface.DbUpdate("exampletable1",data);
+    }
+
+    [Synchronous]
+    public static int Delete(string id)
+    {
+        //We Need to Delete a Row, and Return Number of Affected Rows. Id is Received from Arma
+        return IoC.DBInterface.WriteRaw($"DELETE FROM `exampletable1` WHERE `Id` = '{id}'");
+    }
+}
--- Runner/HiveRunner.cs
+++ Runner/HiveRunner.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Text;
+using ArmaTools.ArrayParser;
+using ArmaTools.ArrayParser.DataTypes;
+using Hive;
 
 namespace Runner
 {
@@ -7,10 +10,83 @@ namespace Runner
     {
         static void Main()
         {
-            var sb = new StringBuilder();
-            Hive.RVEntry.RVExtension(sb,1000,"[\"System\",\"Setup\",false]");
-            Console.WriteLine(sb);
+            if (!Setup())
+                throw new Exception("Hive Failed to Setup!");
+            
+            HiveRunnerExample();
+            
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Exit..");
             Console.ReadLine();
+
+            
+        }
+        
+        static ArmaArray SendToExtension(string controller, string method, ArmaTypeBase parameters)
+        {
+            try
+            {
+                var result = new StringBuilder();
+                RVEntry.RVExtension(result,10240,$"[\"{controller}\",\"{method}\",{parameters}]");
+                return Parser.ArrayFromString(result.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new ArmaArray(false);
+            }
+            
+        }
+
+        static bool Setup()
+        {
+            var result = SendToExtension("System", "Setup", new ArmaBool(false));
+            Console.WriteLine($"Setup Result: {result}");
+            return result.SelectBool(0);
+        }
+
+        static void HiveRunnerExample()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Insert a Row..");
+            Console.ReadKey();
+            var createResult = SendToExtension("Example", "Create",
+                new ArmaArray("ExampleString", 50.25, new ArmaArray(1, "Hello", 2.5, false, "World")));
+            var createResultId = createResult.SelectString(1);
+            Console.WriteLine($"Created Id: {createResultId}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Get Data From the Id..");
+            Console.ReadKey();
+            var singleData = SendToExtension("Example", "GetSingle", new ArmaString(createResultId));
+            Console.WriteLine("Data From GetSingle Call:");
+            Console.WriteLine(singleData.SelectArray(1));
+
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Create Another Row..");
+            Console.ReadKey();
+            var createResult2 = SendToExtension("Example", "Create",
+                new ArmaArray("ExampleString2", 25.50, new ArmaArray(1, "Hello", 5.2, true, "Again")));
+            Console.WriteLine($"Created Id: {createResult2.SelectString(1)}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Update the First Row we Inserted..");
+            Console.ReadKey();
+            
+            SendToExtension("Example", "Update",
+                new ArmaArray(createResultId,"ExampleStringUpdated", null, null,null));
+
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Get All Rows..");
+            Console.ReadKey();
+            var getAllRowsResult = SendToExtension("Example", "GetAll",new ArmaArray());
+            Console.WriteLine(getAllRowsResult.SelectArray(1));
+            
+            Console.WriteLine("Press Any Key to Delete the Second Row we Inserted..");
+            Console.ReadKey();
+            var deleteResult = SendToExtension("Example", "Delete",new ArmaString(createResult2.SelectString(1)));
+            Console.WriteLine($"Rows Affected: {deleteResult.SelectNumber(1)}");
+
         }
     }
 }
