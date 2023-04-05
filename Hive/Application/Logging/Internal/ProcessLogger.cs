c766cf4e201fd4864262ee5eac329762bdcea699
IsGoose <47721300+IsGoose@users.noreply.github.com>
2023-04-05 05:56:21 +0100

    Pass ProcessLogger.Log() on to Thread
    This is quite a Slow Operation, so its best we Pass this on to a Background Thread as to not Halt the Caller Thread

--- Hive/Application/Logging/Internal/ProcessLogger.cs
+++ Hive/Application/Logging/Internal/ProcessLogger.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
 using Hive.Application.Enums;
 using Hive.Application.Extern;
 
@@ -54,86 +55,90 @@ public class ProcessLogger : IInternalLogger
     
     private void Log(string log,LogLevel logLevel)
     {
-        FileLogger.Log(logLevel is LogLevel.Fatal or LogLevel.Error ? "Error" : "Log", $"{DateTime.Now:HH:mm:ss} [{Enum.GetName(typeof(LogLevel), logLevel)}] {log}", logLevel);
-        if (logLevel < IoC.Configuration.LogLevel)
-            return;
-        
-        var colourRef = logLevel switch
+        Task.Run(() =>
         {
-            //Green
-            LogLevel.Trace or LogLevel.Debug or LogLevel.Info => new COLORREF(0xBB,0xBB,0xBB ),
-            //Orange
-            LogLevel.Warn => new COLORREF( 0xFF,0x99,0x33 ),
-            //Red
-            LogLevel.Error or LogLevel.Fatal or _ => new COLORREF( 0xFF, 0x00, 0x00)
-        };
+            FileLogger.Log(logLevel is LogLevel.Fatal or LogLevel.Error ? "Error" : "Log",
+                $"{DateTime.Now:HH:mm:ss} [{Enum.GetName(typeof(LogLevel), logLevel)}] {log}", logLevel);
+            if (logLevel < IoC.Configuration.LogLevel)
+                return;
 
-        //This is All Very Slow, This Should ALWAYS be Passed on to a Background Thread..
-        lock (_lock)
-        {
-            var lineCount = (int)Win32.SendMessage(_textBoxHandle, Win32.EM_GETLINECOUNT, 0, 0);
-            var lastPoint = -1;
-            while (lineCount > Win32.MAX_LINE_COUNT)
+            var colourRef = logLevel switch
             {
-                FINDTEXTEXA fta;
-                fta.chrg.cpMin = lastPoint + 1;
-                fta.chrg.cpMax = -1;
-                fta.lpstrText = "\r";
-                fta.chrgText.cpMin = -1;
-                fta.chrgText.cpMax = -1;
-                var ftaAlloc = GCHandle.Alloc(fta);
-                if (Win32.SendMessage(_textBoxHandle, Win32.EM_FINDTEXTEX, Win32.FR_DOWN | Win32.FR_MATCHCASE,
-                        GCHandle.ToIntPtr(ftaAlloc)) == -1)
-                {
-                    ftaAlloc.Free();
-                    break;
-                }
-                    
-                lastPoint = fta.chrgText.cpMax;
-                lineCount--;
-                ftaAlloc.Free();
-            }
-
-            Win32.SendMessage(_textBoxHandle, Win32.WM_SETREDRAW, false, 0);
-            Win32.SendMessage(_textBoxHandle, Win32.EM_SETSEL, -1, -1);
-
-            var colourData = new CHARFORMATW
-            {
-                crTextColor = colourRef,
-                dwMask = Win32.CFM_COLOR
+                //Green
+                LogLevel.Trace or LogLevel.Debug or LogLevel.Info => new COLORREF(0xBB, 0xBB, 0xBB),
+                //Orange
+                LogLevel.Warn => new COLORREF(0xFF, 0x99, 0x33),
+                //Red
+                LogLevel.Error or LogLevel.Fatal or _ => new COLORREF(0xFF, 0x00, 0x00)
             };
-            colourData.cbSize = (uint)Marshal.SizeOf(colourData);
 
-            if (logLevel is LogLevel.Fatal or LogLevel.Error)
+            //This is All Very Slow, This Should ALWAYS be Passed on to a Background Thread..
+            lock (_lock)
             {
-                colourData.dwMask |= Win32.CFM_BOLD;
-                colourData.dwEffects = Win32.CFE_BOLD;
+                var lineCount = (int)Win32.SendMessage(_textBoxHandle, Win32.EM_GETLINECOUNT, 0, 0);
+                var lastPoint = -1;
+                while (lineCount > Win32.MAX_LINE_COUNT)
+                {
+                    FINDTEXTEXA fta;
+                    fta.chrg.cpMin = lastPoint + 1;
+                    fta.chrg.cpMax = -1;
+                    fta.lpstrText = "\r";
+                    fta.chrgText.cpMin = -1;
+                    fta.chrgText.cpMax = -1;
+                    var ftaAlloc = GCHandle.Alloc(fta);
+                    if (Win32.SendMessage(_textBoxHandle, Win32.EM_FINDTEXTEX, Win32.FR_DOWN | Win32.FR_MATCHCASE,
+                            GCHandle.ToIntPtr(ftaAlloc)) == -1)
+                    {
+                        ftaAlloc.Free();
+                        break;
+                    }
+
+                    lastPoint = fta.chrgText.cpMax;
+                    lineCount--;
+                    ftaAlloc.Free();
+                }
+
+                Win32.SendMessage(_textBoxHandle, Win32.WM_SETREDRAW, false, 0);
+                Win32.SendMessage(_textBoxHandle, Win32.EM_SETSEL, -1, -1);
+
+                var colourData = new CHARFORMATW
+                {
+                    crTextColor = colourRef,
+                    dwMask = Win32.CFM_COLOR
+                };
+                colourData.cbSize = (uint)Marshal.SizeOf(colourData);
+
+                if (logLevel is LogLevel.Fatal or LogLevel.Error)
+                {
+                    colourData.dwMask |= Win32.CFM_BOLD;
+                    colourData.dwEffects = Win32.CFE_BOLD;
+                }
+
+                var colourPtr = Marshal.AllocHGlobal(Marshal.SizeOf(colourData));
+                Marshal.StructureToPtr(colourData, colourPtr, false);
+
+                Win32.SendMessage(_textBoxHandle, Win32.EM_SETCHARFORMAT, Win32.SCF_SELECTION, colourPtr);
+
+                SETTEXTEX text;
+                text.flags = 0xFF;
+                text.codepage = 1200;
+                log = $"{DateTime.Now:H:mm:ss} [{Enum.GetName(typeof(LogLevel), logLevel)}] {log}\r\n";
+
+                var textPtr = Marshal.AllocHGlobal(Marshal.SizeOf(text));
+                Marshal.StructureToPtr(text, textPtr, false);
+                Win32.SendMessageW(_textBoxHandle, Win32.EM_SETTEXTEX, textPtr, log.ToCharArray());
+
+                Marshal.FreeHGlobal(textPtr);
+
+                Win32.SendMessage(_textBoxHandle, Win32.WM_SETREDRAW, true, 0);
+                Win32.SendMessage(_textBoxHandle, Win32.EM_SETSEL, -1, -1);
+                Win32.SendMessage(_textBoxHandle, Win32.EM_SCROLLCARET, 0, 0);
+                Win32.RedrawWindow(_textBoxHandle, IntPtr.Zero, IntPtr.Zero,
+                    RedrawWindowFlags.Erase | RedrawWindowFlags.Frame | RedrawWindowFlags.Invalidate |
+                    RedrawWindowFlags.UpdateNow | RedrawWindowFlags.AllChildren);
+
             }
-            
-            var colourPtr = Marshal.AllocHGlobal(Marshal.SizeOf(colourData));
-            Marshal.StructureToPtr(colourData, colourPtr, false);
-            
-            Win32.SendMessage(_textBoxHandle, Win32.EM_SETCHARFORMAT, Win32.SCF_SELECTION, colourPtr);
-
-            SETTEXTEX text;
-            text.flags = 0xFF;
-            text.codepage = 1200;
-            log = $"{DateTime.Now:H:mm:ss} [{Enum.GetName(typeof(LogLevel), logLevel)}] {log}\r\n";
-
-            var textPtr = Marshal.AllocHGlobal(Marshal.SizeOf(text));
-            Marshal.StructureToPtr(text, textPtr, false);
-            Win32.SendMessageW(_textBoxHandle, Win32.EM_SETTEXTEX, textPtr, log.ToCharArray());
-
-            Marshal.FreeHGlobal(textPtr);
-
-            Win32.SendMessage(_textBoxHandle, Win32.WM_SETREDRAW, true, 0);
-            Win32.SendMessage(_textBoxHandle, Win32.EM_SETSEL, -1, -1);
-            Win32.SendMessage(_textBoxHandle, Win32.EM_SCROLLCARET, 0, 0);
-            Win32.RedrawWindow(_textBoxHandle, IntPtr.Zero, IntPtr.Zero,
-                RedrawWindowFlags.Erase | RedrawWindowFlags.Frame | RedrawWindowFlags.Invalidate |
-                RedrawWindowFlags.UpdateNow | RedrawWindowFlags.AllChildren);
-
-        }
+        });
     }
 
 }
